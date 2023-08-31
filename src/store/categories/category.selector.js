10bd96e9e4f9d27e76a552d989b9b9e2890d6ae4
AndresFSalcedo <84711845+AndresFSalcedo@users.noreply.github.com>
2023-08-31 17:28:15 +1200

    Migrate all the context to Redux
    Use Memoization to effectively render/re-render components, selectors to get data from the store and give the shape that we want

---
+++ src/store/categories/category.selector.js
@@ -0,0 +1,24 @@
+import { createSelector } from 'reselect';
+
+// Get the categories from the root reducer.
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  // Input selector.
+  // Get the slice of the state that this selector is interested in.
+  [selectCategoryReducer],
+  // Output selector
+  // Get the categories from the category reducer.  
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  // Memoize selectCategories(). If it does not change, it will not run the method.
+  [selectCategories],
+  // Method.
+  (categories) => categories.reduce((acc, category) => {
+    const { title, items } = category;
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {})
+);
